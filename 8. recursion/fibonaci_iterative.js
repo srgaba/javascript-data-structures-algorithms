120171615bddbf66678d0b094152240bdf337d60
srgaba <gabrielcmo2018@gmail.com>
2021-02-04 22:35:23 -0300

    *

---
+++ 8. recursion/fibonaci_iterative.js
@@ -0,0 +1,8 @@
+function fibonacciIterative(n) {
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[n];
+}
+fibonacciIterative(3);
---
+++ 8. recursion/fibonaci_recursive.js
@@ -0,0 +1,8 @@
+function fibonacciRecursive(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+fibonacciRecursive(6);
